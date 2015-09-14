de2e40d2395a740ad43ca1c4a3def92b7e3544f1
edcaron <eduardocaron10@gmail.com>
2015-09-14 19:13:22 -0300

    demo 1

---
+++ cliente/QIT-Cliente/src/controle/CapturarSERR.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package regedit;
+
+/**
+ *
+ * @author eduar_000
+ */
+public class CapturarSOUT {
+    
+    
+//            OutputStream output = new FileOutputStream("D:/out.txt", true);
+//
+//        PrintStream out = new QITPrintStream(output);
+//
+//        System.setOut(out);
+//        System.setErr(out);
+//
+//        String[] a = new String[1];
+//        a[2] = "a";
+}
---
+++ cliente/QIT-Cliente/src/controle/QITPrintStream.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+/**
+ *
+ * @author eduar_000
+ */
+public class QITPrintStream extends PrintStream {
+
+    public QITPrintStream(OutputStream out) {
+        super(out);
+    }
+
+    @Override
+    public void print(String s) {
+        super.print(modelo.Util.getCurrentDate() + s);
+    }
+
+    @Override
+    public void println(String x) {
+        super.println(modelo.Util.getCurrentDate() + x);
+    }
+
+}
